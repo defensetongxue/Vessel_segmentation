c47615d4ac34fd2b20166a837d8c44bb0916322a
guoyi <1900013009@pku.edu.cn>
2023-03-06 15:47:44 +0800

    add trainer

--- function.py
+++ function.py
@@ -3,6 +3,9 @@ the file is the utils funtion for training process
 '''
 import torch
 from .loss import dice_loss as loss_function
+import os
+import numpy as np
+from sklearn.metrics import roc_auc_score
 
 def train(model, dataloader, optimizer, device):
     model.train()
@@ -33,3 +36,74 @@ def validate(model, dataloader, device):
         val_loss /= len(dataloader.dataset)
     return val_loss
 
+class AverageMeter(object):
+    def __init__(self):
+        self.initialized = False
+        self.val = None
+        self.avg = None
+        self.sum = None
+        self.count = None
+
+    def initialize(self, val, weight):
+        self.val = val
+        self.avg = val
+        self.sum = np.multiply(val, weight)
+        self.count = weight
+        self.initialized = True
+
+    def update(self, val, weight=1):
+        if not self.initialized:
+            self.initialize(val, weight)
+        else:
+            self.add(val, weight)
+
+    def add(self, val, weight):
+        self.val = val
+        self.sum = np.add(self.sum, np.multiply(val, weight))
+        self.count = self.count + weight
+        self.avg = self.sum / self.count
+
+    @property
+    def value(self):
+        return np.round(self.val, 4)
+
+    @property
+    def average(self):
+        return np.round(self.avg, 4)
+def get_metrics(predict, target, threshold=None, predict_b=None):
+    predict = torch.sigmoid(predict).cpu().detach().numpy().flatten()
+    if predict_b is not None:
+        predict_b = predict_b.flatten()
+    else:
+        predict_b = np.where(predict >= threshold, 1, 0)
+    if torch.is_tensor(target):
+        target = target.cpu().detach().numpy().flatten()
+    else:
+        target = target.flatten()
+    tp = (predict_b * target).sum()
+    tn = ((1 - predict_b) * (1 - target)).sum()
+    fp = ((1 - target) * predict_b).sum()
+    fn = ((1 - predict_b) * target).sum()
+    auc = roc_auc_score(target, predict)
+    acc = (tp + tn) / (tp + fp + fn + tn)
+    pre = tp / (tp + fp)
+    sen = tp / (tp + fn)
+    spe = tn / (tn + fp)
+    iou = tp / (tp + fp + fn)
+    f1 = 2 * pre * sen / (pre + sen)
+    return {
+        "AUC": np.round(auc, 4),
+        "F1": np.round(f1, 4),
+        "Acc": np.round(acc, 4),
+        "Sen": np.round(sen, 4),
+        "Spe": np.round(spe, 4),
+        "pre": np.round(pre, 4),
+        "IOU": np.round(iou, 4),
+    }
+
+def get_instance(module, name, config, *args):
+    return getattr(module, config[name]['type'])(*args, **config[name]['args'])
+
+def dir_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
---
+++ trainer.py
@@ -0,0 +1,142 @@
+import os
+import time
+from datetime import datetime
+import cv2
+import torch
+import numpy as np
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torchvision.transforms.functional as TF
+from loguru import logger
+from tqdm import tqdm
+from function import get_instance,dir_exists,get_metrics,AverageMeter
+
+
+class Trainer:
+    def __init__(self, model, CFG=None, loss=None, train_loader=None, val_loader=None):
+        self.CFG = CFG
+        if self.CFG.amp is True:
+            self.scaler = torch.cuda.amp.GradScaler(enabled=True)
+        self.loss = loss
+        self.model = nn.DataParallel(model.cuda())
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.optimizer = get_instance(
+            torch.optim, "optimizer", CFG, self.model.parameters())
+        self.lr_scheduler = get_instance(
+            torch.optim.lr_scheduler, "lr_scheduler", CFG, self.optimizer)
+        start_time = datetime.now().strftime('%y%m%d%H%M%S')
+        self.checkpoint_dir = os.path.join(
+            CFG.save_dir, self.CFG['model']['type'], start_time)
+        dir_exists(self.checkpoint_dir)
+        cudnn.benchmark = True
+
+    def train(self):
+        for epoch in range(1, self.CFG.epochs + 1):
+            self._train_epoch(epoch)
+            if self.val_loader is not None :
+                results = self._valid_epoch(epoch)
+            if epoch % self.CFG.save_period == 0:
+                self._save_checkpoint(epoch)
+
+    def _train_epoch(self, epoch):
+        self.model.train()
+        wrt_mode = 'train'
+        self._reset_metrics()
+        tic = time.time()
+        for img, gt in self.train_loader:
+            self.data_time.update(time.time() - tic)
+            img = img.cuda(non_blocking=True)
+            gt = gt.cuda(non_blocking=True)
+            self.optimizer.zero_grad()
+            
+            with torch.cuda.amp.autocast(enabled=True):
+                pre = self.model(img)
+                loss = self.loss(pre, gt)
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+           
+            self.total_loss.update(loss.item())
+            self.batch_time.update(time.time() - tic)
+            
+            self._metrics_update(
+                *get_metrics(pre, gt, threshold=self.CFG.threshold).values())
+            print(
+                'TRAIN ({}) | Loss: {:.4f} | AUC {:.4f} F1 {:.4f} Acc {:.4f}  Sen {:.4f} Spe {:.4f} Pre {:.4f} IOU {:.4f} |B {:.2f} D {:.2f} |'.format(
+                    epoch, self.total_loss.average, *self._metrics_ave().values(), self.batch_time.average, self.data_time.average))
+            tic = time.time()
+        self.lr_scheduler.step()
+
+    def _valid_epoch(self, epoch):
+        logger.info('\n###### EVALUATION ######')
+        self.model.eval()
+        wrt_mode = 'val'
+        self._reset_metrics()
+        tbar = tqdm(self.val_loader, ncols=160)
+        with torch.no_grad():
+            for img, gt in tbar:
+                img = img.cuda(non_blocking=True)
+                gt = gt.cuda(non_blocking=True)
+                with torch.cuda.amp.autocast(enabled=True):
+                    predict = self.model(img)
+                    loss = self.loss(predict, gt)
+                self.total_loss.update(loss.item())
+                self._metrics_update(
+                    *get_metrics(predict, gt, threshold=self.CFG.threshold).values())
+                tbar.set_description(
+                    'EVAL ({})  | Loss: {:.4f} | AUC {:.4f} F1 {:.4f} Acc {:.4f} Sen {:.4f} Spe {:.4f} Pre {:.4f} IOU {:.4f} |'.format(
+                        epoch, self.total_loss.average, *self._metrics_ave().values()))
+                
+        log = {
+            'val_loss': self.total_loss.average,
+            **self._metrics_ave()
+        }
+        return log
+
+    def _save_checkpoint(self, epoch):
+        state = {
+            'arch': type(self.model).__name__,
+            'epoch': epoch,
+            'state_dict': self.model.state_dict(),
+            'optimizer': self.optimizer.state_dict(),
+            'config': self.CFG
+        }
+        filename = os.path.join(self.checkpoint_dir,
+                                f'checkpoint-epoch{epoch}.pth')
+        logger.info(f'Saving a checkpoint: {filename} ...')
+        torch.save(state, filename)
+        return filename
+
+    def _reset_metrics(self):
+        self.batch_time = AverageMeter()
+        self.data_time = AverageMeter()
+        self.total_loss = AverageMeter()
+        self.auc = AverageMeter()
+        self.f1 = AverageMeter()
+        self.acc = AverageMeter()
+        self.sen = AverageMeter()
+        self.spe = AverageMeter()
+        self.pre = AverageMeter()
+        self.iou = AverageMeter()
+        self.CCC = AverageMeter()
+    def _metrics_update(self, auc, f1, acc, sen, spe, pre, iou):
+        self.auc.update(auc)
+        self.f1.update(f1)
+        self.acc.update(acc)
+        self.sen.update(sen)
+        self.spe.update(spe)
+        self.pre.update(pre)
+        self.iou.update(iou)
+
+    def _metrics_ave(self):
+
+        return {
+            "AUC": self.auc.average,
+            "F1": self.f1.average,
+            "Acc": self.acc.average,
+            "Sen": self.sen.average,
+            "Spe": self.spe.average,
+            "pre": self.pre.average,
+            "IOU": self.iou.average
+        }
