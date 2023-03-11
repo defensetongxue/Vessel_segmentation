71955701a9b37b575cf7d809d34786396bcc00e8
guoyi <1900013009@pku.edu.cn>
2023-03-11 11:45:37 +0800

    bugging

--- VesselSegModule/__init__.py
+++ VesselSegModule/__init__.py
@@ -1,2 +1,2 @@
 # this moudle is the export interface for the main project ROP dignose
-from vessel_seg_processer import VesselSegProcesser
+from .vessel_seg_processer import VesselSegProcesser
--- VesselSegModule/vessel_seg_processer.py
+++ VesselSegModule/vessel_seg_processer.py
@@ -9,15 +9,24 @@ import numpy as np
 
 
 class VesselSegProcesser():
-    def __init__(self, model_name, save_path, path, resize=(512, 512)) -> None:
-        self.model = getattr(models, model_name)
+    def __init__(self, model_name, save_path,  resize=(512, 512)) :
+        self.model = getattr(models, model_name)()
         checkpoint = torch.load(os.path.join(
-            path, 'VesselSegModule', 'checkpoint/best.pth'))
+            './VesselSegModule', 'checkpoint/best.pth'))
         self.model.load_state_dict(checkpoint['state_dict'])
+        self.model.cuda()
+
         self.save_path = save_path
-        self.reszie = resize
-        # self.mask=
+        self.resize = resize
+
+        # generate mask
+        mask=Image.open('mask.png')
+        mask=transforms.Resize(self.resize)(mask)
+        mask=transforms.ToTensor()(mask)[0] 
+        self.mask=mask
+
         self.transforms = transforms.Compose([
+            transforms.Resize((512,512)),
             transforms.Grayscale(1),
             transforms.ToTensor(),
             transforms.Normalize([0.3968], [0.1980])
@@ -29,19 +38,21 @@ class VesselSegProcesser():
         # open the image and preprocess
         img = Image.open(img_path)
         img = self.transforms(img)
-        img = cv2.resize(
-            img, self.reszie, interpolation=cv2.INTER_LINEAR)
 
         # generate predic vascular with pretrained model
+        img=img.unsqueeze(0)# as batch size 1
         pre = self.model(img.cuda())
-        pre = transforms.functional.cop(0, 0, *self.resize)
+        pre = transforms.functional.crop(pre,0, 0, *self.resize)
         pre = pre[0, 0, ...]
-        predict = torch.sigmoid(pre).cpu().detach().numpy()
+        predict = torch.sigmoid(pre).cpu().detach()
+
+        # mask 
+        predict=torch.where(self.mask<0.1,self.mask,predict)
 
         # save the image
         file_name = os.path.basename(img_path)
         file_name = file_name.split('.')[0]
         cv2.imwrite(
             os.path.join(self.save_path, "{}_vs.png".format(file_name)),
-            np.uint8(predict*255))
+            np.uint8(predict.numpy()*255))
         return
